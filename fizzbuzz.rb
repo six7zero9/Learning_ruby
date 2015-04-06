c9b3dc2343d772a8a02f976d66fc1fbf7b648129
Michael Jackson <michaelpauljackson@gmail.com>
2015-04-06 11:16:52 -0500

    fizzbuzz practice

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(num1, num2)
+  (num1..num2).each do |num|
+    if num % 3 == 0
+      if num % 5 == 0
+        p "fizzbuzz"
+      else
+        p "fizz"
+      end
+    elsif num % 5 == 0
+      p "buzz"
+    else
+      p num
+    end
+  end
+end
+
+fizzbuzz(2,100)
